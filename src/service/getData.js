8d42e7990892f3a4ce1c347d929d452db66f7f71
sunjing <sjfanasn@sina.com>
2018-09-28 09:10:56 +0800

    完成首页

--- src/service/getData.js
+++ src/service/getData.js
@@ -1,15 +1,17 @@
 import fetch from '../config/fetch'
 import { getStore } from '../config/mUtils'
 import axios from 'axios';
+
+const baseUri = 'http://cangdu.org:8001/'
 /**
  * 获取首页默认地址
  */
-export const cityGuess = () => { return axios.get('http://cangdu.org:8001/v1/cities?type=guess') }
+export const cityGuess = () => { return axios.get(baseUri+'v1/cities?type=guess') }
 /**
  * 获取首页热门城市
  */
-export const hotcity = () => { return axios.get('http://cangdu.org:8001/v1/cities?type=hot') }
+export const hotcity = () => { return axios.get(baseUri+'v1/cities?type=hot') }
 /**
  * 获取首页所有城市
  */
-export const groupcity = () => { return axios.get('http://cangdu.org:8001/v1/cities?type=group') }
+export const groupcity = () => { return axios.get(baseUri+'v1/cities?type=group') }
